b10d54b0e0f88a0fc709bc86b143c66af1ba77d8
Jaken Herman <jakenherman7@gmail.com>
2021-04-07 19:40:57 -0500

    Now getting scores for mlb, nba, nhl, nfl, wnba

--- src/ScoreProvider.ts
+++ src/ScoreProvider.ts
@@ -1,56 +1,68 @@
 import * as vscode from 'vscode';
 import * as axios from 'axios';
 
+class Event {
+    public competitors: Array<Object> = [];
+    public shortName: string = '';
+    public competitions: Array<any> = [];
+}
+
 export class ScoreProvider implements vscode.TreeDataProvider<Score> {
-    private scoreboard: string;
-    constructor(scoreboard: string) { 
-        this.scoreboard = scoreboard;
+  onDidChangeTreeData?: vscode.Event<Score|null|undefined>|undefined;
+
+  data: any;
+  url: string;
+
+  constructor(url: string) {
+    this.url = url;
+    this.data = this.getScores().then(res => {
+      return res;
+    });
+  }
+
+  getScores(): Thenable<Score[]> {
+      return axios.default.get(this.url)
+        .then(res => {
+            const data: Array<Score> = [];
+            const events = res.data.events;
+            events.forEach((x: Event) => {
+                const teams = x.shortName.split(' @ ');
+                const homeTeam = teams[1];
+                const awayTeam = teams[0];
+
+                const homeTeamScore = x.competitions[0].competitors.find((t: any) => t.homeAway === 'home').score;
+                const awayTeamScore = x.competitions[0].competitors.find((t: any) => t.homeAway === 'away').score;
+
+                data.push(new Score(`${awayTeam} (${awayTeamScore}) @ ${homeTeam} (${homeTeamScore})`));
+            });
+            return data;
+        })
+        .catch(e => {
+            console.error(e);
+            return [new Score('Error obtaining scores...')];
+        });
+  }
+
+  getTreeItem(element: Score): vscode.TreeItem|Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(element?: Score|undefined): vscode.ProviderResult<Score[]> {
+    if (element === undefined) {
+      return this.data;
     }
-
-    getTreeItem(score: Score): vscode.TreeItem {
-        return score;
-    }
-
-    getChildren(score?: Score): any {
-        if (false) {
-            // todo: this will be for errors later.
-            vscode.window.showInformationMessage('No scores found...');
-            return Promise.resolve([]);
-        }
-
-        return [
-            new Score('test', vscode.TreeItemCollapsibleState.Expanded)
-        ];
-
-        return axios.default.get(this.scoreboard)
-            .then(res => {
-                return [new Score(res.data, vscode.TreeItemCollapsibleState.Expanded)];
-            })
-            .catch(e => {
-                return [new Score('Error', vscode.TreeItemCollapsibleState.Collapsed)];
-            })
-    }
-
-    private _onDidChangeTreeData: vscode.EventEmitter<Score | undefined | null | void> = new vscode.EventEmitter<Score | undefined | null | void>();
-    readonly onDidChangeTreeData: vscode.Event<Score | undefined | null | void> = this._onDidChangeTreeData.event;
-
-    refresh(): void {
-        this._onDidChangeTreeData.fire();
-    }
+    return element.children;
+  }
 }
 
 class Score extends vscode.TreeItem {
-    constructor(
-        public readonly label: string,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState
-    ) {
-        super(label, collapsibleState);
-        this.tooltip = 'test';
-        this.description = 'desc';
-    }
-    
-    iconPath = {
-        light: '',
-        dark: ''
-    };
+  children: Score[]|undefined;
+
+  constructor(label: string, children?: Score[]) {
+    super(
+        label,
+        children === undefined ? vscode.TreeItemCollapsibleState.None :
+                                 vscode.TreeItemCollapsibleState.Expanded);
+    this.children = children;
+  }
 }
--- src/links.ts
+++ src/links.ts
@@ -1,1 +1,5 @@
 export const mlb = 'http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard';
+export const nhl = 'http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard';
+export const nba = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
+export const wnba = 'http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard';
+export const nfl = 'http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard';
